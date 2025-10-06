d49d71bd9fbd310b600351797c287ae79276fba2
Eduardo Guilherme Dias <eduardogdias.edu@gmail.com>
2025-10-06 14:41:17 -0300

    Salvando no SQL Server, script sql

--- script_bd.sql
+++ script_bd.sql
@@ -24,3 +24,14 @@ CREATE TABLE tb_chamada (
     CONSTRAINT fk_chamada_senha FOREIGN KEY (senha_fk) REFERENCES tb_senha(id),
     CONSTRAINT fk_chamada_guiche FOREIGN KEY (guiche_fk) REFERENCES tb_guiche(id)
 );
+
+
+
+
+-- populando as tabelas
+
+INSERT INTO tb_guiche (numero, atendente, livre) VALUES ('01', 'Juliano Freitas', 1);
+INSERT INTO tb_guiche (numero, atendente, livre) VALUES ('02', 'Raquel Silva', 1);
+INSERT INTO tb_guiche (numero, atendente, livre) VALUES ('03', 'Abel Lima', 1);
+INSERT INTO tb_guiche (numero, atendente, livre) VALUES ('04', 'Fernando Gustavo', 1);
+INSERT INTO tb_guiche (numero, atendente, livre) VALUES ('05', 'Maria Fernanda', 1);
--- src/main/resources/db/migration/V2__insert-guiches.sql
+++ src/main/resources/db/migration/V2__insert-guiches.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_guiche (numero, atendente, livre) VALUES ('01', 'Juliano Freitas', TRUE);
-INSERT INTO tb_guiche (numero, atendente, livre) VALUES ('02', 'Raquel Silva', TRUE);
-INSERT INTO tb_guiche (numero, atendente, livre) VALUES ('03', 'Abel Lima', TRUE);
-INSERT INTO tb_guiche (numero, atendente, livre) VALUES ('04', 'Fernando Gustavo', TRUE);
-INSERT INTO tb_guiche (numero, atendente, livre) VALUES ('05', 'Maria Fernanda', TRUE);
+INSERT INTO tb_guiche (numero, atendente, livre) VALUES ('01', 'Juliano Freitas', 1);
+INSERT INTO tb_guiche (numero, atendente, livre) VALUES ('02', 'Raquel Silva', 1);
+INSERT INTO tb_guiche (numero, atendente, livre) VALUES ('03', 'Abel Lima', 1);
+INSERT INTO tb_guiche (numero, atendente, livre) VALUES ('04', 'Fernando Gustavo', 1);
+INSERT INTO tb_guiche (numero, atendente, livre) VALUES ('05', 'Maria Fernanda', 1);
